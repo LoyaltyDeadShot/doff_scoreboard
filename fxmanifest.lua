a37d372bed213fbed26969bd77990972ed6b4e69
Daróczi Vilmos <=>
2023-04-03 12:51:30 +0200

    Create empty script

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'DoFF'
+description 'DoFF Scoreboard for ESX Legacy'
+lua54 'yes'
+version '1.0.0'
+
+shared_scripts {
+	'@es_extended/imports.lua',
+	'@es_extended/locale.lua',
+	'config.lua',
+	'locales/*.lua',
+}
+
+server_scripts {
+	'server/main.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/js/script.js',
+	'html/css/style.css',
+}
+
+dependency 'es_extended'
